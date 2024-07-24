9f6b7ffda80d55b8b1cfcac8f34e1791bb5394ef
akshayjthummar <akshaythummar34@gmail.com>
2024-07-24 16:42:17 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('welcome to auth service');
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
+    next();
 });
 
 export default app;
