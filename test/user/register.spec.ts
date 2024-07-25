95bc834b3dd62047ed8cffbd56f917702455c7c2
akshayjthummar <akshaythummar34@gmail.com>
2024-07-25 14:18:12 +0530

    add test role asign

--- test/user/register.spec.ts
+++ test/user/register.spec.ts
@@ -4,8 +4,8 @@ import request from 'supertest';
 import app from '../../src/app';
 import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../src/config/data-source';
-import { truncateTables } from '../utils';
 import { User } from '../../src/entity/User';
+import { Roles } from '../../src/constants';
 
 describe('POST /auth/register', () => {
     let connection: DataSource;
@@ -13,7 +13,8 @@ describe('POST /auth/register', () => {
         connection = await AppDataSource.initialize();
     });
     beforeEach(async () => {
-        await truncateTables(connection);
+        await connection.dropDatabase();
+        await connection.synchronize();
     });
     afterAll(async () => {
         await connection.destroy();
@@ -59,6 +60,20 @@ describe('POST /auth/register', () => {
             expect(user[0].firstName).toBe(userdata.firstName);
             expect(user[0].lastName).toBe(userdata.lastName);
             expect(user[0].email).toBe(userdata.email);
+        });
+
+        it('should assign a customer role', async () => {
+            const userdata = {
+                firstName: 'akshay',
+                lastName: 'thummar',
+                email: 'akshaythummar34@gmail.com',
+                password: 'secret',
+            };
+            await request(app).post('/auth/register').send(userdata);
+            const useRepository = connection.getRepository(User);
+            const users = await useRepository.find();
+            expect(users[0]).toHaveProperty('role');
+            expect(users[0].role).toBe(Roles.CUSTOMER);
         });
     });
 
