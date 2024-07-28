12937f48422ac426ea6ffb5fbbf463dc8699387e
akshayjthummar <akshaythummar34@gmail.com>
2024-07-28 16:53:32 +0530

    add login endpoint

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -5,12 +5,15 @@ import { Logger } from 'winston';
 import { validationResult } from 'express-validator';
 import { JwtPayload } from 'jsonwebtoken';
 import { TokenService } from '../services/TokenService';
+import createHttpError from 'http-errors';
+import { CredentialService } from '../services/CredentialService';
 
 export class AuthControllers {
     constructor(
         private userService: UserService,
         private logger: Logger,
         private tokenService: TokenService,
+        private credentialPassword: CredentialService,
     ) {}
     async register(
         req: RegisterUserRequest,
@@ -35,7 +38,6 @@ export class AuthControllers {
                 email,
                 password,
             });
-            this.logger.info('User has been registerd');
 
             const payload: JwtPayload = {
                 sub: String(user.id),
@@ -65,7 +67,83 @@ export class AuthControllers {
                 maxAge: 1000 * 60 * 60 * 24 * 365, //365day,
                 httpOnly: true,
             });
+            this.logger.info('User has been registerd', { id: user.id });
             res.status(201).json({ id: user.id });
+        } catch (error) {
+            next(error);
+            return;
+        }
+    }
+
+    async login(req: RegisterUserRequest, res: Response, next: NextFunction) {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+        const { email, password } = req.body;
+        this.logger.debug('New Request to login a user', {
+            email,
+            password: '******',
+        });
+        // Check if username(email) is exist in db
+        // Compare Password
+        // Ganarate Token
+        // Add token to cookie
+        // Return the response (id)
+        try {
+            const user = await this.userService.findByEmail(email);
+            if (!user) {
+                const err = createHttpError(
+                    400,
+                    'Email or password does not match',
+                );
+                next(err);
+                return;
+            }
+
+            const passwordMatch = await this.credentialPassword.comparePassword(
+                password,
+                user.password,
+            );
+            if (!passwordMatch) {
+                const err = createHttpError(
+                    400,
+                    'Email or password does not match',
+                );
+                next(err);
+                return;
+            }
+
+            const payload: JwtPayload = {
+                sub: String(user.id),
+                role: user.role,
+            };
+
+            const accessToken = this.tokenService.ganerateAccessToken(payload);
+
+            // Persist the refresh token
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
+            const refreshToken = this.tokenService.ganerateRefreshToken({
+                ...payload,
+                id: String(newRefreshToken.id),
+            });
+
+            res.cookie('accessToken', accessToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60, //1h,
+                httpOnly: true,
+            });
+
+            res.cookie('refreshToken', refreshToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60 * 24 * 365, //365day,
+                httpOnly: true,
+            });
+            this.logger.info('user has been logged in', { id: user.id });
+            res.status(200).json({ id: user.id });
         } catch (error) {
             next(error);
             return;
